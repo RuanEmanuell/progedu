ee4303fd98fd91f1cbd304d960dd7822b54d84c9
Ruan Emanuell <ruanvitorcom540@gmail.com>
2022-11-16 21:16:10 -0300

    refactor: separating similar widgets

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,3 +1,4 @@
+import 'package:alarme/widgets/loginregisterwidgets/mainbutton.dart';
 import "package:flutter/material.dart";
 import "package:firebase_auth/firebase_auth.dart";
 import 'package:google_fonts/google_fonts.dart';
@@ -5,7 +6,9 @@ import 'package:provider/provider.dart';
 
 import '../controller/controller.dart';
 
-import "../widgets/input.dart";
+import '../widgets/loginregisterwidgets/maintext.dart';
+import '../widgets/loginregisterwidgets/logo.dart';
+import '../widgets/loginregisterwidgets/input.dart';
 
 import "../screens/home.dart";
 
@@ -18,6 +21,26 @@ class LoginScreen extends StatelessWidget {
     var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
 
+    loginAction() async {
+      try {
+        await FirebaseAuth.instance
+            .signInWithEmailAndPassword(email: emailController.text, password: passwordController.text)
+            .then(((value) {
+          Navigator.push(context, MaterialPageRoute(
+            builder: (context) {
+              return HomeScreen();
+            },
+          ));
+        }));
+      } on FirebaseAuthException catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: Colors.red,
+            content: Text(e.message == "Given String is empty or null"
+                ? "One or more fields are empty"
+                : e.message!)));
+      }
+    }
+
     return Scaffold(
         backgroundColor: const Color.fromARGB(255, 56, 56, 56),
         body: Consumer<Controller>(builder: (context, value, child) {
@@ -25,19 +48,8 @@ class LoginScreen extends StatelessWidget {
             return SingleChildScrollView(
                 child: Stack(children: [
               Column(children: [
-                SafeArea(
-                  child: Container(
-                      margin: EdgeInsets.only(top: screenHeight / 60, bottom: screenHeight / 60),
-                      height:
-                          orientation == Orientation.portrait ? screenHeight / 3.5 : screenHeight / 2.25,
-                      child: Image.asset("assets/images/logo.png")),
-                ),
-                Text("/ProgEdu",
-                    style: GoogleFonts.vt323(
-                        fontSize: screenWidth / 7, color: const Color.fromARGB(255, 0, 255, 8))),
-                Text("Welcome back!",
-                    style: GoogleFonts.vt323(
-                        fontSize: screenWidth / 15, color: const Color.fromARGB(255, 0, 255, 8))),
+                LogoWidget(orientation: orientation),
+                MainText(text: "Welcome back!"),
                 SizedBox(height: screenHeight / 60),
                 InputWidget(controller: emailController, hintText: "Type your email...", value: value),
                 InputWidget(
@@ -50,35 +62,12 @@ class LoginScreen extends StatelessWidget {
                       child: Text("Forgot your password?",
                           style: GoogleFonts.vt323(fontSize: screenWidth / 20, color: Colors.yellow))),
                 ),
-                Container(
-                  margin: EdgeInsets.only(
-                      top: screenHeight / 40, right: screenWidth / 15, left: screenWidth / 15),
-                  width: screenWidth,
-                  height: orientation == Orientation.portrait ? screenHeight / 10 : screenHeight / 5,
-                  child: ElevatedButton(
-                    style:
-                        ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 0, 206, 7)),
-                    onPressed: () async {
-                      try {
-                        await FirebaseAuth.instance.signInWithEmailAndPassword(
-                            email: emailController.text, password: passwordController.text);
-
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) {
-                            return HomeScreen();
-                          },
-                        ));
-                      } on FirebaseAuthException catch (e) {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            backgroundColor: Colors.red,
-                            content: Text(e.message == "Given String is empty or null"
-                                ? "One or more fields are empty"
-                                : e.message!)));
-                      }
+                MainButton(
+                    onPressed: () {
+                      loginAction();
                     },
-                    child: Text("Login", style: GoogleFonts.vt323(fontSize: screenWidth / 14)),
-                  ),
-                ),
+                    orientation: orientation,
+                    text: "Login"),
                 Container(
                   margin: EdgeInsets.only(top: screenHeight / 40, bottom: screenHeight / 40),
                   child: InkWell(
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -7,7 +7,10 @@ import '../controller/controller.dart';
 
 import "login.dart";
 import "home.dart";
-import "../widgets/input.dart";
+import '../widgets/loginregisterwidgets/maintext.dart';
+import '../widgets/loginregisterwidgets/mainbutton.dart';
+import '../widgets/loginregisterwidgets/logo.dart';
+import '../widgets/loginregisterwidgets/input.dart';
 
 class RegisterScreen extends StatelessWidget {
   var emailController = TextEditingController();
@@ -18,58 +21,44 @@ class RegisterScreen extends StatelessWidget {
     var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
 
+    registerAction() async {
+      try {
+        await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(
+                email: emailController.text, password: passwordController.text)
+            .then(((value) {
+          Navigator.push(context, MaterialPageRoute(
+            builder: (context) {
+              return HomeScreen();
+            },
+          ));
+        }));
+      } on FirebaseAuthException catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: Colors.red,
+            content: Text(e.message == "Given String is empty or null"
+                ? "One or more fields are empty"
+                : e.message!)));
+      }
+    }
+
     return Scaffold(
         backgroundColor: const Color.fromARGB(255, 56, 56, 56),
         body: Consumer<Controller>(builder: (context, value, child) {
           return OrientationBuilder(builder: (context, orientation) {
             return SingleChildScrollView(
                 child: Column(children: [
-              SafeArea(
-                child: Container(
-                    margin: EdgeInsets.only(top: screenHeight / 60, bottom: screenHeight / 60),
-                    height:
-                        orientation == Orientation.portrait ? screenHeight / 3.5 : screenHeight / 2.25,
-                    child: Image.asset("assets/images/logo.png")),
-              ),
-              Text("/ProgEdu",
-                  style: GoogleFonts.vt323(
-                      fontSize: screenWidth / 7, color: const Color.fromARGB(255, 0, 255, 8))),
-              Text("Learn to code now!",
-                  style: GoogleFonts.vt323(
-                      fontSize: screenWidth / 15, color: const Color.fromARGB(255, 0, 255, 8))),
+              LogoWidget(orientation: orientation),
+              MainText(text: "Learn to code now!"),
               SizedBox(height: screenHeight / 60),
               InputWidget(controller: emailController, hintText: "Type an email...", value: value),
               InputWidget(controller: passwordController, hintText: "Type a password...", value: value),
-              Container(
-                margin: EdgeInsets.only(
-                    top: screenHeight / 40, right: screenWidth / 15, left: screenWidth / 15),
-                width: screenWidth,
-                height: orientation == Orientation.portrait ? screenHeight / 10 : screenHeight / 5,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 0, 206, 7)),
-                  onPressed: () async {
-                    try {
-                      await FirebaseAuth.instance
-                          .createUserWithEmailAndPassword(
-                              email: emailController.text, password: passwordController.text)
-                          .then(((value) {
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) {
-                            return HomeScreen();
-                          },
-                        ));
-                      }));
-                    } on FirebaseAuthException catch (e) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          backgroundColor: Colors.red,
-                          content: Text(e.message == "Given String is empty or null"
-                              ? "One or more fields are empty"
-                              : e.message!)));
-                    }
+              MainButton(
+                  onPressed: () {
+                    registerAction();
                   },
-                  child: Text("Register", style: GoogleFonts.vt323(fontSize: screenWidth / 14)),
-                ),
-              ),
+                  orientation: orientation,
+                  text: "Register"),
               Container(
                 margin: EdgeInsets.only(top: screenHeight / 40, bottom: screenHeight / 40),
                 child: InkWell(
